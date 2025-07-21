9403c170137d1f7a17ae423cd8eb70b630009161
Lucas <lk7750785@outlook.com>
2025-07-21 20:16:28 -0300

    refatorar fetch bitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,21 +1,13 @@
-export default function initFetchBitcoin() {
-  const btcUrl = "https://blockchain.info/ticker";
-  const btcText = document.querySelector(".btc-preco");
-
-  async function getBtcValue() {
-    try {
-      const btcResponse = await fetch(btcUrl);
-      const btcJSON = await btcResponse.json();
-      const btcPreco = `R$  ${(1 / btcJSON.BRL.sell).toFixed(7)}`.replace(
-        ".",
-        ","
-      );
-
-      btcText.innerText = btcPreco;
-    } catch (erro) {
-      console.log(Error(erro));
-    }
-  }
-
-  getBtcValue();
+export default function fetchBitcoin(url, target) {
+  const btcText = document.querySelector(target);
+  fetch(url)
+    .then((btcResponse) => btcResponse.json())
+    .then(
+      (btcJSON) =>
+        (btcText.innerText = `R$  ${(1 / btcJSON.BRL.sell).toFixed(7)}`.replace(
+          ".",
+          ","
+        ))
+    )
+    .catch((erro) => console.log(Error(erro)));
 }
